d9fd55369f6fdca03cf9b4fbece54256878e8a8f
christopher <chris.aholou@gmail.com>
2022-05-12 04:27:04 +0200

    part2

--- src/components/ChatComponent.vue
+++ src/components/ChatComponent.vue
@@ -1,30 +1,31 @@
 <template>
-    <div class="container">
+    <div class="container mt-5">
         <div class="card_avatar" v-for="items in message" :key="items.id">
-            <img src="../assets/images/avatar-5.jpg" alt="avatar" class="size-avatar">
-            <div class="card_text_avatar">
-            <h6>{{items.name}}</h6>
-            <p>{{items.description}}</p>
+            <div class="card_header">
+                <img src="../assets/images/avatar-5.jpg" alt="avatar" class="size-ico-avatar">
+                <h6 class="card_name">{{items.name}}</h6>
+                <img src="../assets/images/trash.png" alt="supprimer" class ="size-ico-trash">
             </div>
-            <img src="../assets/images/trash.png" alt="supprimer" class ="size-trash">
+            <div class="card-text">
+                <p> {{items.description}}</p>
+                <img class ="imageUser" alt="img utilisateur" v-if=" items.imageUrl != null " :src="items.imageUrl">
+            </div>
         </div>
         <!--newMessages-->
-        <form v-on:submit="submitMessage">
+        <form v-on:submit.prevent="submitMessage" enctype="multipart/form-data">
             <div id = "cardInputNewMessage">
-<!--            <input type="text" placeholder="new message" name="message" id ="new-message" value=""> -->
                 <div class="form-group">
-                    <label for="name">Name</label>
-                    <input type="text" class="form-control" id="name" placeholder="Your name" require v-model="form.name">
+                    <label for="name">{{name}}</label>
                 </div>
-                <textarea id="new-message" placeholder="add multiple lines" v-model="form.newMessage"></textarea>
-                <input type="file" class="inputFile">
+                <textarea id="newMessage" name="newMessage" placeholder="add multiple lines" v-model="newMessage"></textarea>
+                <input type="file" ref="file" name="userfile" id="userfile" @change="selectFile" class="inputFile">
                 <img class="imgStyle" src="../assets/images/avatar-5.jpg" v-show="falsed">
                 <button type="submit"  class="btn-add">Envoyer</button>
             </div>
          </form>
         <!--count&recherche-->
         <div id ="cardCountMessageAndSeach">
-            <p id= "Compter-Message"> Messages total: {{counter}} </p>
+            <p id= "Compter-Message"> Messages total: {{count}}</p>
             <input type="text" placeholder="searchMessage" name="searchMessage" id ="searchMessage" value="">
             <button type="submit" id="btn-search">Search</button>
         </div>
@@ -34,32 +35,132 @@
 <script>
 
 const axios = require('axios').default
+
+
 export default {
   name: 'ChatComponent',
-  props: ['message'],
   data () {
     return {
-      form: {
         newMessage: '',
-        name: ''
-      }
+        name: localStorage.getItem('userName'),
+        userfile: '',
+        message: [],
+        count: 0
     }
   },
-  methods: {  
-    submitMessage(){ 
-        console.log(this.form.newMessage)
-        console.log(this.form.name)
+  created (){ this.fetchGetMessage() },
 
-      axios.post('http://localhost:3000/api/chat', this.form)
-        .then((res) => { console.log(res.json) })
-        .catch((error) => { console.log(error) }) 
+  methods: { 
+      // Get message 
+    async fetchGetMessage (){
+        let userId = localStorage.getItem('userId');
+        let token = localStorage.getItem('token');
+        await axios.get(`http://localhost:3000/api/chat?id=${userId}`,{ headers: { authorization: `BEARER ${token}` }} )
+            .then((res)=> { 
+                 this.message = res.data
+                 this.count = res.data.length 
+            }).catch((err)=>{ throw err})
+        console.log(this.message)
+    },
+      
+    // Post  message 
+   submitMessage(){ 
+        let userId = localStorage.getItem('userId');
+        let token = localStorage.getItem('token'); 
+
+        let formData = new FormData();
+        formData.append('name', this.name);
+        formData.append('newMessage', this.newMessage);
+        formData.append('image', this.userfile);
+        
+        let form = {newMessage: this.newMessage, name: this.name, image: this.userfile }
+
+        console.log(formData);
+        
+        for (var value of formData.values()) {
+                console.log("X= " + value);
+        }
+
+
+        axios.post(`http://localhost:3000/api/chat?id=${userId}`,formData,  { headers: { authorization: `BEARER ${token}` }}  )
+            .then((res) => {  res.status(200) })
+            .catch((error) => { console.log(error) }) 
+
+        this.newMessage =""
+
+
+    },
+    selectFile(){
+              this.userfile = this.$refs.file.files[0];  
+    },
+
+    //supression d'un message
+    deleteMessage(){
+        axios.delete('url supp message ')
     }
-  }
+  
+       
+  }    
+
 }
 
 </script>
 
-<style>
+<style scoped>
+.container{
+    font-family:'Open Sans', sans-serif;
+    font-size: 15px;
+}
+.imageUser{
+    max-width: 300px;
+    max-height: 300px;
+}
+.card_avatar
+    {
+        display: flex;
+        flex-direction: column;
+        width: auto;
+        height: 30%;
+        border: 1px solid  #DADADA;
+        background-color: #FFFFFF;
+        padding: 0.75rem ;
+        margin-bottom: 1rem;
+    }
+    .card_header{
+        display: flex;
+        align-items: center;
+        margin-top: 0.75rem;
+        height: 4.25rem;
+        margin-bottom: 0.75rem;
+
+    }
+    
+    .card-text
+    {  margin-top: 0.75rem;
+       overflow-wrap: break-word;
+    }
+    .card-text p {
+        text-align: left;
+        margin: 0.75rem;
+    }
+    .card_name{
+        margin: 0.75rem;
+        font-size: 18px;
+        font-weight: bold;
+    }
+    
+     .size-ico-trash
+    {
+        height: 20px;
+        
+    }
+    .size-ico-avatar
+    {
+        height: 100px;
+        border-radius: 100px;
+    }
+   
+
     .inputFile{
         margin: 5px;
     }
@@ -83,18 +184,7 @@ export default {
         padding: 20px;
         justify-content: space-between;
     }
-    .card_avatar
-    {
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        border: 1px solid  #DADADA;
-        background-color: #FFFFFF;
-        height: auto;
-        padding: 20px;
-    }
-    .card-text-avatar
-    {  padding-left: 20px; }
+    
     #cardInputNewMessage
     {
         display:flex;
@@ -109,7 +199,7 @@ export default {
         margin: auto;
 
     }
-    #new-message
+    #newMessage
     {
         width: 60%;
         margin-left: 5px;
@@ -139,27 +229,6 @@ export default {
         margin-top: 20px;
         margin-bottom: 20px;
     }
-    p
-    {
-        font-family:'Open Sans', sans-serif;
-        margin: 0px;
-        font-size: 14px;
+    
 
-    }
-    h6
-    {
-        margin: 0px;
-        padding-bottom: 0px;
-        font-size: 14px;
-    }
-    .size-trash
-    {
-        height: 15px;
-
-    }
-    .size-avatar
-    {
-        height: 60px;
-        border-radius: 100px;
-    }
 </style>
