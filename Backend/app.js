2d1803327d6e12efec582e5f96c448985eec6409
christopher <chris.aholou@gmail.com>
2022-06-06 14:53:22 +0200

    erreur corigier

--- Backend/app.js
+++ Backend/app.js
@@ -1,23 +1,19 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mysql = require('mysql');
-require('dotenv').config();
 const path = require("path");
+require('dotenv').config();
 
-//declarations process connection a la base de donner
-/* let host = process.env.HOST;
-let userLogin = process.env.USER_LOGIN_SQL;
-let password = process.env.PASSWORD_LOGIN_SQL; */
+let password = process.env.S3_PASSWORD_LOGIN_SQL;
+let login = process.env.S3_USER_LOGIN_SQL;
+let iphost = process.env.S3_HOST;
 
 const db = mysql.createConnection({
-    host : '127.0.0.1', //localhost
-    user : 'root', //nom_utilisateur
-    password : 'loulou', //mot_de_passe_utilisateur
+    host : iphost, //localhost
+    user : login, //nom_utilisateur
+    password : password, //mot_de_passe_utilisateur
     database : "groupomania" // nom_BDD
 });
-
-
-
 db.connect(function(err){
     if(err){
         console.log("Connexion à Mysql échoué !")
@@ -28,28 +24,18 @@ db.connect(function(err){
 
 const chatRoutes = require('./routes/chat');
 const userRoutes = require('./routes/user');
-
-
-
 const app =  express();
 
 app.use(express.json());
-
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
     next();
 });
-
- 
 app.use(bodyParser.json());
-
 app.use('/images', express.static(path.join(__dirname, 'images')));
-
-
 app.use('/api/chat', chatRoutes);
 app.use('/api/user', userRoutes);
-
 
 module.exports = app;
